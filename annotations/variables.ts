0b4862680402148a14b12d1fba41ed285cb6dc05
Tyler Pritchard <pritchard.tyler@gmail.com>
2023-06-27 20:36:46 -0700

    classes

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -1,2 +1,28 @@
+// primative
 let apples: number = 5;
 let speed: string = 'fast';
+let hasName: boolean = true;
+
+//falsy
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+//built in objects
+let now: Date = new Date();
+
+//object literals
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumber: number[] = [1, 2, 3];
+let myBooleans: boolean[] = [true, false, true];
+
+//classes
+class Car{
+
+};
+
+let car: Car = new Car();
+
+let point: {'x':number, 'y':number} = {
+    'x': 10,
+    'y': 20
+}
