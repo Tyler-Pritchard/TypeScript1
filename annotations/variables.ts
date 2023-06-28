07c22d3e3d97dfa683311c46323a4b5f8d8feeb0
Tyler Pritchard <pritchard.tyler@gmail.com>
2023-06-27 21:09:29 -0700

    inference failures

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -39,3 +39,23 @@ const logNumber: (i: number) => void = (i: number) => {
 //1) When the 'any' type is returned
 const json = '{"x": 10, "y": 20}';
 const coordinates: {x: number; y: number} = JSON.parse(json);
+
+//2 Declaring variable on one line and initializing it later
+let words = ['red', 'blue', 'green'];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+    if (words[i] == 'green') {
+        foundWord = true;
+    }
+}
+
+//3 Variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (var i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 0) {
+        numberAboveZero = numbers[i];
+    }
+}
