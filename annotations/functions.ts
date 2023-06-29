f1729cfb0d598e47c8205f75f31f59127cacdb2c
Tyler Pritchard <pritchard.tyler@gmail.com>
2023-06-28 20:36:34 -0700

    functions

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -1,3 +1,15 @@
 const add = (a: number, b: number): number => {
-    return a+b
+    return a + b;
+}
+
+const subtract = (a: number, b: number): number => {
+    return a - b;
+}
+
+function divide(a: number, b: number): number {
+    return a / b;
+}
+
+const multiply = (a: number, b: number): number => {
+    return a * b;
 }
