831895bed374f4bfa12aa9eaabc9a4565d0d1d85
Tyler Pritchard <pritchard.tyler@gmail.com>
2023-06-29 18:10:46 -0700

    tuples

--- annotations/arrays.ts
+++ annotations/arrays.ts
@@ -8,9 +8,15 @@ const car = carMakers[0];
 const myCar = carMakers.pop();
 
 // prevent incompatible values
-carMakers.push(100)
+carMakers.push(100);
 
 // Help with .map
 carMakers.map((car: string): string => {
     return car.toUpperCase();
 });
+
+// flexible types
+const importantDates: (Date | string)[] = [new Date(), '10-10-2030'];
+importantDates.push('10-12-2030');
+importantDates.push(new Date());
+importantDates.push(100);
