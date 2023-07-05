17706a7d06724f62eac90b38b7c03bdad8f7f071
Tyler Pritchard <pritchard.tyler@gmail.com>
2023-07-05 16:04:46 -0700

    constructors

--- classes.ts
+++ classes.ts
@@ -1,22 +1,24 @@
 class Vehicle {
+    constructor(public color: string) {}
+
     protected honk(): void {
         console.log("beep beep");
     }
 };
 
-const vehicle = new Vehicle();
-// vehicle.honk();
+const vehicle = new Vehicle('orange');
+console.log(vehicle.color)
 
-class Car extends Vehicle {
-    private drive(): void {
-        console.log("go car go");
-    }
-    startDriving(): void {
-        this.drive();
-        this.honk();
-    }
+// class Car extends Vehicle {
+//     private drive(): void {
+//         console.log("go car go");
+//     }
+//     startDriving(): void {
+//         this.drive();
+//         this.honk();
+//     }
 
-};
+// };
 
-const car = new Car();
-car.startDriving();
+// const car = new Car();
+// car.startDriving();
